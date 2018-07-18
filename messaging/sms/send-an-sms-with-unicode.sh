8f859e4dae99e15304ba785d2cba07d504db855b
Lorna Jane Mitchell <lorna@lornajane.net>
2018-07-18 15:04:45 +0100

    Tidy up variables to make example runnable

--- messaging/sms/send-an-sms-with-unicode.sh
+++ messaging/sms/send-an-sms-with-unicode.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+source "../../config.sh"
+
 curl -X "POST" "https://rest.nexmo.com/sms/json" \
-  -d "from=FROM" \
-  -d "to=TO_NUMBER" \
+  -d "from=AcmeInc" \
+  -d "to=$TO_NUMBER" \
   -d "text=こんにちは世界" \
   -d "type=unicode" \
-  -d "api_key=NEXMO_API_KEY" \
-  -d "api_secret=NEXMO_API_SECRET"
+  -d "api_key=$NEXMO_API_KEY" \
+  -d "api_secret=$NEXMO_API_SECRET"
 
