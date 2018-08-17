56578821c0bd0404fa3290494dcf8b55285d1606
Tony Bedford <tony.bedford@vonage.com>
2018-08-17 10:01:01 +0100

    Add get event types block plus remove unnecessary comments from other audit scripts.

--- audit/get-event-types.sh
+++ audit/get-event-types.sh
@@ -2,5 +2,5 @@
 
 source "../../config.sh"
 
-curl "https://api.nexmo.com/beta/audit/events" \
+curl -X "OPTIONS" "https://api.nexmo.com/beta/audit/events" \
      -u 'NEXMO_API_KEY:NEXMO_API_SECRET'
--- audit/get-event.sh
+++ audit/get-event.sh
@@ -2,8 +2,6 @@
 
 source "../../config.sh"
 
-## Get Audit Events
-# Fetches specified audit event
 curl "https://api.nexmo.com/beta/audit/events/EVENT_UUID" \
      -u 'NEXMO_API_KEY:NEXMO_API_SECRET'
 
