64f85a05a0648038bb3fef2c9aefd0283b36c196
Lorna Jane Mitchell <lorna@lornajane.net>
2018-08-20 11:17:56 +0100

    Add trailing spaces to curl code sample

--- voice/voice-api/make-an-outbound-call.sh
+++ voice/voice-api/make-an-outbound-call.sh
@@ -2,9 +2,9 @@
 source "../../config.sh"
 source "../../jwt.sh"
 
-curl -X POST https://api.nexmo.com/v1/calls\
-  -H "Authorization: Bearer "$JWT\
-  -H "Content-Type: application/json"\
+curl -X POST https://api.nexmo.com/v1/calls \
+  -H "Authorization: Bearer $JWT" \
+  -H "Content-Type: application/json" \
   -d '{"to":[{"type": "phone","number": "'$TO_NUMBER'"}],
       "from": {"type": "phone","number": "'$NEXMO_NUMBER'"},
       "answer_url":["https://developer.nexmo.com/ncco/tts.json"]}'
