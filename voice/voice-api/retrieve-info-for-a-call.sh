53584d7646aa06f8c4adca4b390b7fcc81331823
Lorna Jane Mitchell <lorna@lornajane.net>
2018-08-21 15:36:10 +0100

    Simplify getting info for a call

--- voice/voice-api/retrieve-info-for-a-call.sh
+++ voice/voice-api/retrieve-info-for-a-call.sh
@@ -2,14 +2,6 @@
 source "../../config.sh"
 source "../../jwt.sh"
 
-UUID=$(curl -s -X POST https://api.nexmo.com/v1/calls\
-  -H "Authorization: Bearer "$JWT\
-  -H "Content-Type: application/json"\
-  -d '{"to":[{"type": "phone","number": "'$TO_NUMBER'"}],
-      "from": {"type": "phone","number": "'$NEXMO_NUMBER'"},
-      "answer_url":["https://developer.nexmo.com/ncco/tts.json"]}' \
-      | awk -F "[,:]" '/uuid/{print substr($2,2,length($2)-2)}')
-
 curl "https://api.nexmo.com/v1/calls/"$UUID \
   -H "Authorization: Bearer "$JWT \
 
