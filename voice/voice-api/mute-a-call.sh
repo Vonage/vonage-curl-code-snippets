ec8e091555162dcc27d555cfcc110bff88c087da
Lorna Jane Mitchell <lorna@lornajane.net>
2018-08-21 15:26:36 +0100

    Add mute, simplify examples rather than repeating the make call step

--- voice/voice-api/mute-a-call.sh
+++ voice/voice-api/mute-a-call.sh
@@ -5,11 +5,11 @@ source "../../jwt.sh"
 curl -X PUT https://api.nexmo.com/v1/calls/$UUID \
   -H "Authorization: Bearer "$JWT\
   -H "Content-Type: application/json"\
-  -d '{"action": "earmuff"}'
+  -d '{"action": "mute"}'
 
 sleep 5s
 
 curl -X PUT https://api.nexmo.com/v1/calls/$UUID \
   -H "Authorization: Bearer "$JWT\
   -H "Content-Type: application/json"\
-  -d '{"action": "unearmuff"}'
+  -d '{"action": "unmute"}'
