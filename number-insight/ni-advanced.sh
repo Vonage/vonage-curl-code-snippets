8a0556a2ed47ea2bcf011a5fc0fd3b811e1cc243
Mark Lewin <mark.lewin@vonage.com>
2018-09-19 13:59:10 +0100

    Add curl building blocks

---
+++ number-insight/ni-advanced.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source "../config.sh"
+
+curl -X "POST" "https://api.nexmo.com/ni/advanced/json" \
+     -d "api_key=$NEXMO_API_KEY" \
+     -d "api_secret=$NEXMO_API_SECRET" \
+     -d "number=$NEXMO_TO_LOOKUP"
