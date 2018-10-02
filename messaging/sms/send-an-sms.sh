9182522c8fd9b1b8fe4d871ea68aeef1f7766a27
Sam Machin <sam.machin@nexmo.com>
2018-10-02 13:28:26 +0100

    Update send-an-sms.sh
    Updated to remove quotes as the current version doens't work on OSX at least

--- messaging/sms/send-an-sms.sh
+++ messaging/sms/send-an-sms.sh
@@ -2,8 +2,8 @@
 source "../../config.sh"
 
 curl -X "POST" "https://rest.nexmo.com/sms/json" \
-  -d "from=AcmeInc" \
-  -d "text=A text message sent using the Nexmo SMS API" \
-  -d "to=$TO_NUMBER" \
-  -d "api_key=$NEXMO_API_KEY" \
-  -d "api_secret=$NEXMO_API_SECRET"
+  -d from=AcmeInc \
+  -d text="A text message sent using the Nexmo SMS API" \
+  -d to=$TO_NUMBER \
+  -d api_key=$NEXMO_API_KEY \
+  -d api_secret=$NEXMO_API_SECRET
