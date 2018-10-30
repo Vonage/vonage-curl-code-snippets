a376e5b8bdfc85b84bf59198b212439e38b0422f
stark <thestarknight@tutanota.com>
2018-10-30 21:12:37 +0530

    config.sh: added support for environment variables (#24)

--- config.sh
+++ config.sh
@@ -1,42 +1,42 @@
 # General
-NEXMO_API_KEY=""
-NEXMO_API_SECRET=""
+NEXMO_API_KEY=${NEXMO_API_KEY:-""}
+NEXMO_API_SECRET=${NEXMO_API_SECRET:-""}
 
-NEXMO_APPLICATION_ID=""
-NEXMO_PRIVATE_KEY=""
+NEXMO_APPLICATION_ID=${NEXMO_APPLICATION_ID:-""}
+NEXMO_PRIVATE_KEY=${NEXMO_PRIVATE_KEY:-""}
 
-TO_NUMBER=""
-FROM_NUMBER=""
-NEXMO_NUMBER=""
+TO_NUMBER=${TO_NUMBER:-""}
+FROM_NUMBER=${FROM_NUMBER:-""}
+NEXMO_NUMBER=${NEXMO_NUMBER:-""}
 
 # For Facebook messaging
-FB_SENDER_ID=""
-FB_RECIPIENT_ID=""
+FB_SENDER_ID=${FB_SENDER_ID:-""}
+FB_RECIPIENT_ID=${FB_RECIPIENT_ID:-""}
 
 # For Viber messaging
-VIBER_SERVICE_MESSAGE_ID=""
+VIBER_SERVICE_MESSAGE_ID=${VIBER_SERVICE_MESSAGE_ID:-""}
 
 # For WhatsApp messaging
-WHATSAPP_NUMBER=""
+WHATSAPP_NUMBER=${WHATSAPP_NUMBER:-""}
 
-ACCOUNT_ID="" # For APIs that allow you to access the resources of a sub account
+ACCOUNT_ID=${ACCOUNT_ID:-""} # For APIs that allow you to access the resources of a sub account
 
 # Account API
-SECRET_ID=""
+SECRET_ID=${SECRET_ID:-""}
 
 # Audit API
-EVENT_UUID=""  # UUID of audit event
-SEARCH_TEXT="number"
-DATE_FROM="2018-07-01"
-DATE_TO="2018-08-01"
+EVENT_UUID=${EVENT_UUID:-""}  # UUID of audit event
+SEARCH_TEXT=${SEARCH_TEXT:-"number"}
+DATE_FROM=${DATE_FROM:-"2018-07-01"}
+DATE_TO=${DATE_TO:-"2018-08-01"}
 
 # Number Insight API
-INSIGHT_NUMBER=""
-WEBHOOK_URL=""
+INSIGHT_NUMBER=${INSIGHT_NUMBER:-""}
+WEBHOOK_URL=${WEBHOOK_URL:-""}
 
 # MMS
-US_SHORT_CODE=""
-IMG_URL=""
+US_SHORT_CODE=${US_SHORT_CODE:-""}
+IMG_URL=${IMG_URL:-""}
 
 # If we have a local config, override using that
 CONFIG_DIR=$(dirname "${BASH_SOURCE[0]}")
