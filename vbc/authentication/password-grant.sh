f27fbb45bafb882a3eb0578876b4ce71f2ed487a
Mark Lewin <mark.lewin@vonage.com>
2019-03-12 10:58:57 +0000

    Use production URLs

--- vbc/authentication/password-grant.sh
+++ vbc/authentication/password-grant.sh
@@ -3,4 +3,4 @@ source "../../config.sh"
 
 curl -k -d "grant_type=password&username=$VBC_USERNAME&password=$VBC_PASSWORD" \
         -d "&client_id=$VBC_CLIENT_ID&client_secret=$VBC_CLIENT_SECRET" \
-        https://api.entva0.qa.vonagenetworks.net/token
+        https://api.vonage.com/token
