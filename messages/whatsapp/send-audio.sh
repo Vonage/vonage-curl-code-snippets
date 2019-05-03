573761c3a3152caa97ffb0ea2b00e00dbe1c8839
Tony Bedford <tony.bedford@vonage.com>
2019-05-03 14:23:11 +0100

    Reord into subdir and add WhatsApp media snippets.

---
+++ messages/whatsapp/send-audio.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+source "../config.sh"
+source "../jwt.sh"
+
+curl -X POST https://api.nexmo.com/v0.1/messages \
+  -H 'Authorization: Bearer '$JWT \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: application/json' \
+  -d $'{
+    "from": { "type": "whatsapp", "number": '$WHATSAPP_NUMBER' },
+    "to": { "type": "whatsapp", "number": '$TO_NUMBER' },
+    "message": {
+      "content": {
+        "type": "audio",
+        "audio": { 
+          "url": '$AUDIO_URL',
+        }
+      }
+    }
+  }'
