5d7cbac5451c5c7f071c81126d920ed9a911be94
Tony Bedford <tony.bedford@vonage.com>
2019-05-10 14:24:03 +0100

    Add capabilities

--- application/create-application.sh
+++ application/create-application.sh
@@ -6,5 +6,19 @@ curl -X "POST" "https://api.nexmo.com/v2/applications" \
      -H 'Content-Type: application/json' \
      -u "$NEXMO_API_KEY:$NEXMO_API_SECRET" \
      -d $'{
-  "name": "Code Snippets V2 Application"
+  "name": "Code Snippets V2 Application",
+  "capabilities": {
+    "messages": {
+      "webhooks": {
+        "inbound_url": {
+          "address": "https://example.com/webhooks/inbound",
+          "http_method": "POST"
+        },
+        "status_url": {
+          "address": "https://example.com/webhooks/status",
+          "http_method": "POST"
+        }
+      }
+    }
+  }
 }'
