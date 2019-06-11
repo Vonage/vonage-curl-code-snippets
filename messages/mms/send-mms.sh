9f9d604caea6a23af668bb18fedbb36e23a68f29
Tony Bedford <tony.bedford@vonage.com>
2019-06-11 11:56:50 +0100

    Use JWT to be consistent with other messages scripts.

--- messages/mms/send-mms.sh
+++ messages/mms/send-mms.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 source "../../config.sh"
+source "../../jwt.sh"
 
 curl -X POST https://api.nexmo.com/v0.1/messages \
-  -u "$NEXMO_API_KEY:$NEXMO_API_SECRET" \
+  -H 'Authorization: Bearer '$JWT\
   -H 'Content-Type: application/json' \
   -H 'Accept: application/json' \
-  -d '{ 
+  -d $'{ 
    "from":{
         "type": "mms",    
         "number": "'$FROM_NUMBER'"
