dceb28e8d1a6fab1ceae767efc8850cd1272fb79
sarwarbd07 <53219245+sarwarbd07@users.noreply.github.com>
2019-07-23 17:04:36 +0600

    send-an-sms.php

---
+++ messaging/sms/send-an-sms.php
@@ -0,0 +1,6 @@
+curl -X "POST" "https://rest.nexmo.com/sms/json" \
+     -d "from=Nexmo" \
+     -d "text=Hello from Nexmo" \
+     -d "to=8801711982172" \
+     -d "api_key=0f579d56" \
+     -d "api_secret=kYhVYZiIPTYmSzG3"
--- messaging/sms/send-an-sms.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source "../../config.sh"
-
-curl -X "POST" "https://rest.nexmo.com/sms/json" \
-  -d "from=AcmeInc" \
-  -d "text=A text message sent using the Nexmo SMS API" \
-  -d "to=$TO_NUMBER" \
-  -d "api_key=$NEXMO_API_KEY" \
-  -d "api_secret=$NEXMO_API_SECRET"
