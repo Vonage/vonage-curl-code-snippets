33b2415714edb7ebdeac4658932ff675d1c701f5
Tony Bedford <tony.bedford@vonage.com>
2019-07-29 13:31:52 +0100

    Fixed missing option letter

--- subaccounts/suspend-subaccount.sh
+++ subaccounts/suspend-subaccount.sh
@@ -4,4 +4,4 @@ source "../config.sh"
 
 curl -X PATCH -u $API_KEY:$API_SECRET https://api.nexmo.com/accounts/$API_KEY/subaccounts/$SUBACCOUNT_KEY  \
      -H "Content-Type: application/json"  \
-     - $'{"suspended":TRUE}'
+     -d $'{"suspended":TRUE}'
