beaa79816247db40770e20c7387762e229ff05e1
Tony Bedford <tony.bedford@vonage.com>
2019-07-29 13:16:14 +0100

    Remove debug code.

--- subaccounts/create-subaccount.sh
+++ subaccounts/create-subaccount.sh
@@ -2,9 +2,6 @@
 
 source "../config.sh"
 
-echo $NEW_SUB_NAME
-echo $NEW_SUB_SECRET
-
 curl -X POST -u $API_KEY:$API_SECRET https://api.nexmo.com/accounts/$API_KEY/subaccounts  \
      -H "Content-Type: application/json"  \
      -d $'{"name":"$NEW_SUB_NAME", "secret":"$NEW_SUB_SECRET"}'
