dead7091f3345d07587565969cb321db394f9832
Tony Bedford <tony.bedford@vonage.com>
2019-08-01 11:07:44 +0100

    Add correct quoting

--- subaccounts/create-subaccount.sh
+++ subaccounts/create-subaccount.sh
@@ -4,4 +4,4 @@ source "../config.sh"
 
 curl -X POST -u $API_KEY:$API_SECRET https://api.nexmo.com/accounts/$API_KEY/subaccounts  \
      -H "Content-Type: application/json"  \
-     -d $'{"name":"$NEW_SUB_NAME", "secret":"$NEW_SUB_SECRET"}'
+     -d $'{"name":"'$NEW_SUB_NAME'", "secret":"'$NEW_SUB_SECRET'"}'
--- subaccounts/transfer-credit.sh
+++ subaccounts/transfer-credit.sh
@@ -4,4 +4,4 @@ source "../config.sh"
 
 curl -X POST -u $API_KEY:$API_SECRET https://api.nexmo.com/accounts/$API_KEY/credit-transfers  \
      -H "Content-Type: application/json"  \
-     -d $'{"from":"$API_KEY", "to":"$SUBACCOUNT_KEY", "amount": $AMOUNT}'
+     -d $'{"from":"'$API_KEY'", "to":"'$SUBACCOUNT_KEY'", "amount": '$AMOUNT'}'
