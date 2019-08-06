01fb2470028d8fcc77df0aab8e95a2ea6229a1de
Tony Bedford <tony.bedford@vonage.com>
2019-08-06 10:17:21 +0100

    Variable name change for clarity.

--- subaccounts/create-subaccount.sh
+++ subaccounts/create-subaccount.sh
@@ -4,4 +4,4 @@ source "../config.sh"
 
 curl -X POST -u $NEXMO_API_KEY:$NEXMO_API_SECRET https://api.nexmo.com/accounts/$NEXMO_API_KEY/subaccounts  \
      -H "Content-Type: application/json"  \
-     -d $'{"name":"'$NEW_SUB_NAME'", "secret":"'$NEW_SUB_SECRET'"}'
+     -d $'{"name":"'$NEW_SUBACCOUNT_NAME'", "secret":"'$NEW_SUBACCOUNT_SECRET'"}'
