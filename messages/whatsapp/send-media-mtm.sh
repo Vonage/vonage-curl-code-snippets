5547344d8c8d983374e0e5c084a678afab2ad403
Tony Bedford <tony.bedford@vonage.com>
2019-12-09 10:43:22 +0000

    Send media mtm

---
+++ messages/whatsapp/send-media-mtm.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+source "../../config.sh"
+source "../../jwt.sh"
+
+curl -X POST \
+  https://api.nexmo.com/v0.1/messages \
+  -H 'Authorization: Bearer' $JWT \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "from": {
+    "type": "whatsapp",
+    "number": "'$WHATSAPP_NUMBER'"
+  },
+  "to": {
+    "type": "whatsapp",
+    "number": "'$TO_NUMBER'"
+  },
+  "message": {
+    "content": {
+      "type": "custom",
+      "custom": {
+        "type": "template",
+        "template": {
+          "namespace": "whatsapp:hsm:technology:nexmo",
+          "name": "parcel_location",
+          "language": {
+            "policy": "deterministic",
+            "code": "en"
+          },
+          "components": [
+            {
+              "type": "header",
+              "parameters": [
+                {
+                  "type": "location",
+                  "location": {
+                    "longitude": -122.425332,
+                    "latitude": 37.758056,
+                    "name": "Facebook HQ",
+                    "address": "1 Hacker Way, Menlo Park, CA 94025"
+                  }
+                }
+              ]
+            },
+            {
+              "type": "body",
+              "parameters": [
+                {
+                  "type": "text",
+                  "text": "Value 1"
+                },
+                {
+                  "type": "text",
+                  "text": "Value 2"
+                },
+                {
+                  "type": "text",
+                  "text": "Value 3"
+                }
+              ]
+            }
+          ]
+        }
+      }
+    }
+  }
+}'
