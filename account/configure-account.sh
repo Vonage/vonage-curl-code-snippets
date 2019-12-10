d96b04dfbd972e3ab59f874d33d77517b4844b2a
fadirabadi <55899363+fadirabadi@users.noreply.github.com>
2019-12-10 11:12:43 +0200

    Delete configure-account.sh
    <plist version="1.0">
    <dict>
      ...
      <key>FacebookAppID</key>
      <string>{your-app-id}</string>
      <key>AccountKitClientToken</key>
      <string>{your-account-kit-client-token}</string>
      <key>CFBundleURLTypes</key>
      <array>
        <dict>
          <key>CFBundleURLSchemes</key>
          <array>
            <string>ak{your-app-id}</string>
          </array>
        </dict>
      </array>
      ...
    </dict>
    </plist>

--- account/configure-account.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-source "../config.sh"
-
-curl -X POST "https://rest.nexmo.com/account/settings?api_key=$NEXMO_API_KEY&api_secret=$NEXMO_API_SECRET" \
-    -d moCallBackUrl=$SMS_CALLBACK_URL
-
-
