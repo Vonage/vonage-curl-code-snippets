2cdd34726dfde7ab5d7e2868f5cbe00639e8407c
Lorna Jane Mitchell <lorna@lornajane.net>
2020-01-02 17:02:29 +0000

    Add examples for new PSD2 feature in Verify API

---
+++ verify/send-psd2-code.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+source "../config.sh"
+
+curl -X GET "https://api.nexmo.com/verify/psd2/json?&api_key=$NEXMO_API_KEY&api_secret=$NEXMO_API_SECRET&number=$RECIPIENT_NUMBER&payee=AcmeInc&amount=12.34"
