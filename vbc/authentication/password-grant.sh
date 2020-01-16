28dc7b095a295b8d56e67e9740bb763eb43a3469
Tony Hung <tbass134@gmail.com>
2020-01-16 11:55:56 -0500

    updated code snipped to append vbc.prod to username

--- vbc/authentication/password-grant.sh
+++ vbc/authentication/password-grant.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source "../../config.sh"
 
-curl -k -d "grant_type=password&username=$VBC_USERNAME&password=$VBC_PASSWORD" \
+curl -k -d "grant_type=password&username=$VBC_USERNAME@vbc.prod&password=$VBC_PASSWORD" \
         -d "&client_id=$VBC_CLIENT_ID&client_secret=$VBC_CLIENT_SECRET" \
         https://api.vonage.com/token
