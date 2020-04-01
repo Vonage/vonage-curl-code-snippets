405d08cffacf6575f4c8960d408f5d37298bf262
Tony Bedford <tony.bedford@vonage.com>
2020-04-01 10:35:22 +0100

    Improve formatting

--- messages/sandbox/messenger/send-text.sh
+++ messages/sandbox/messenger/send-text.sh
@@ -2,13 +2,13 @@ curl -X POST $MESSAGES_SANDBOX_URL \
    -u $NEXMO_API_KEY:$NEXMO_API_SECRET \
    -H 'Content-Type: application/json' \
    -H 'Accept: application/json' \
-   -d $'{
+   -d '{
      "from": { "type": "messenger", "id": "'$FB_SANDBOX_ID'" },
      "to": { "type": "messenger", "id": "'$FB_RECIPIENT_ID'" },
      "message": {
-                 "content": {
-                 "type": "text",
-                 "text": "Messenger message sent from Messages Sandbox"
+      "content": {
+        "type": "text",
+        "text": "Messenger message sent from Messages Sandbox"
      }
-    }
+   }
  }'
