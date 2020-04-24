db29fc410bc374c466de920c7572fd87558a5810
tea26k <58318532+tea26k@users.noreply.github.com>
2020-04-24 08:16:52 +0700

    ccb0c9d9
    Qci9dvTKCUaFUQgl

--- verify/send-verification-code.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-source "../config.sh"
-
-curl -X GET "https://api.nexmo.com/verify/json?&api_key=$NEXMO_API_KEY&api_secret=$NEXMO_API_SECRET&number=$RECIPIENT_NUMBER&brand=AcmeInc"
