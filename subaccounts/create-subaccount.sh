946bdcbfcd841b9847e99b3aa3ac2eb5b08a609e
tea26k <58318532+tea26k@users.noreply.github.com>
2020-04-24 08:26:26 +0700

    Update create-subaccount.sh

--- subaccounts/create-subaccount.sh
+++ subaccounts/create-subaccount.sh
@@ -5,3 +5,7 @@ source "../config.sh"
 curl -X POST -u $NEXMO_API_KEY:$NEXMO_API_SECRET https://api.nexmo.com/accounts/$NEXMO_API_KEY/subaccounts  \
      -H "Content-Type: application/json"  \
      -d $'{"name":"'$NEW_SUBACCOUNT_NAME'", "secret":"'$NEW_SUBACCOUNT_SECRET'"}'
+curl -X POST -u $NEXMO_API_KEY:$NEXMO_API_SECRET https://api.nexmo.com/accounts/$NEXMO_API_KEY/subaccounts  \
+     -H "Content-Type: application/json"  \
+     -d $'{"name":"'$NEW_SUBACCOUNT_NAME'", "secret":"'$NEW_SUBACCOUNT_SECRET'"}'
+Copy
