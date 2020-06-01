6bff86dbd7ce3dea8be0609d821c5309c9147d0a
Lorna Jane Mitchell <lorna@lornajane.net>
2020-06-01 17:19:30 +0100

    Use POST for Verify PSD2 snippets

--- verify/send-psd2-code-with-workflow.sh
+++ verify/send-psd2-code-with-workflow.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source "../config.sh"
 
-curl -X GET "https://api.nexmo.com/verify/psd2/json?&api_key=$NEXMO_API_KEY&api_secret=$NEXMO_API_SECRET&number=$RECIPIENT_NUMBER&payee=AcmeInc&amount=12.34&workflow_id=$WORKFLOW_ID"
+curl -X POST "https://api.nexmo.com/verify/psd2/json" -d api_key=$NEXMO_API_KEY -d api_secret=$NEXMO_API_SECRET -d number=$RECIPIENT_NUMBER -d payee=AcmeInc -d amount=12.34 -d workflow_id=$WORKFLOW_ID
