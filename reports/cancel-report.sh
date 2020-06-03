f22e40de816e23df92788e30f1b83311c7faa0cd
Tony Bedford <tony.bedford@vonage.com>
2020-06-03 16:35:35 +0100

    Request ID of report execution

--- reports/cancel-report.sh
+++ reports/cancel-report.sh
@@ -3,4 +3,4 @@
 source "../config.sh"
 
 curl -X DELETE -u "$NEXMO_API_KEY:$NEXMO_API_SECRET" \
-     "https://api.nexmo.com/v2/reports/$REPORT_ID
+     "https://api.nexmo.com/v2/reports/$REQUEST_ID
