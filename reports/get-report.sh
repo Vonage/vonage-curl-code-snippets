6845d4dcaf5ca78c2bfd77e62c630cfa3bae210c
Tony Bedford <tony.bedford@live.co.uk>
2020-06-09 14:22:24 +0100

    File ID is more accurate than report ID

--- reports/get-report.sh
+++ reports/get-report.sh
@@ -3,4 +3,4 @@
 source "../config.sh"
 
 curl -o ./report.zip -u "$NEXMO_API_KEY:$NEXMO_API_SECRET" \
-     "https://api.nexmo.com/v3/media/$REPORT_ID"
+     "https://api.nexmo.com/v3/media/$FILE_ID"
