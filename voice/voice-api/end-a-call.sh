ffed1a44f8942f69c05ca4f7fe3c6ef38aea3766
Faiz Azhar <faiz@cloudflare.com>
2020-10-23 21:52:56 +0800

    add hangup voice call

---
+++ voice/voice-api/end-a-call.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+source "../../config.sh"
+source "../../jwt.sh"
+
+curl -X PUT https://api.nexmo.com/v1/calls/$UUID \
+  -H "Authorization: Bearer "$JWT\
+  -H "Content-Type: application/json"\
+  -d '{"action": "hangup"}'
+
