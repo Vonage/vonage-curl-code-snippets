3cfd757045b1ecae5ec97c8256ab06e03d4e05f7
Faiz Azhar <faiz@cloudflare.com>
2020-10-23 21:52:08 +0800

    add example to stop playing after 5s

--- voice/voice-api/play-audio-into-a-call.sh
+++ voice/voice-api/play-audio-into-a-call.sh
@@ -6,3 +6,9 @@ curl -X PUT https://api.nexmo.com/v1/calls/$UUID/stream \
   -H "Authorization: Bearer "$JWT\
   -H "Content-Type: application/json"\
   -d '{"stream_url": ["https://raw.githubusercontent.com/nexmo-community/ncco-examples/gh-pages/assets/welcome_to_nexmo.mp3"]}'
+
+sleep 5s
+
+curl -X DELETE https://api.nexmo.com/v1/calls/$UUID/stream \
+  -H "Authorization: Bearer "$JWT\
+  -H "Content-Type: application/json"\
--- voice/voice-api/play-tts-into-a-call.sh
+++ voice/voice-api/play-tts-into-a-call.sh
@@ -6,3 +6,9 @@ curl -X PUT https://api.nexmo.com/v1/calls/$UUID/talk\
   -H "Authorization: Bearer "$JWT\
   -H "Content-Type: application/json"\
   -d '{"text": "Hello, hi there"}'
+
+sleep 5s
+
+curl -X DELETE https://api.nexmo.com/v1/calls/$UUID/talk\
+  -H "Authorization: Bearer "$JWT\
+  -H "Content-Type: application/json"\
