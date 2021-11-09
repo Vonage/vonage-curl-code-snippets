cb94db9fff893b723e1fed80911bd367a20cddc8
Tonya Camille <tsims25@gmail.com>
2021-11-09 14:13:38 -0600

    Update code snippet send-sms.sh
    For Messages API v1 - Updated code snippet for sending an sms in sen-smh.sh
    
    Related to DEVX-5751
    
    Merge by November 10,2021

--- messages/sms/send-sms.sh
+++ messages/sms/send-sms.sh
@@ -3,17 +3,14 @@
 source "../../config.sh"
 source "../../jwt.sh"
 
-curl -X POST https://api.nexmo.com/v0.1/messages \
+curl -X POST https://api.nexmo.com/v1/messages \
   -H 'Authorization: Bearer '$JWT\
   -H 'Content-Type: application/json' \
   -H 'Accept: application/json' \
   -d $'{
-    "from": { "type": "sms", "number": "'$FROM_NUMBER'" },
-    "to": { "type": "sms", "number": "'$TO_NUMBER'" },
-    "message": {
-      "content": {
-        "type": "text",
-        "text": "This is an SMS sent from the Messages API"
-      }
-    }
-  }'
+          "message_type": "text",
+          "text": "Nexmo Verification code: 12345. Valid for 10 minutes.",
+          "to": "'$TO_NUMBER'",
+          "from": "'$FROM_NUMBER'",
+          "channel": "sms"
+}'
