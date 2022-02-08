d95ba69d6c007a96575c131b98b4dce03b2c63f4
superchilled <karl@superchilled.co.uk>
2022-02-08 15:23:10 +0000

    Further amendments to send-media-mtm messages example for WhatsApp

--- messages/whatsapp/send-media-mtm.sh
+++ messages/whatsapp/send-media-mtm.sh
@@ -7,20 +7,52 @@ curl -X POST $MESSAGES_API_URL \
   -H 'Authorization: Bearer' $JWT \
   -H 'Content-Type: application/json' \
   -d '{
-  "message_type": "template",
-  "template": {
-    "name": "'$WHATSAPP_TEMPLATE_NAMESPACE':'$WHATSAPP_TEMPLATE_NAME'",
-    "parameters": [
-      "Value 1",
-      "Value 2",
-      "Value 3"
-    ]
-  },
+  "message_type": "custom",
   "to": "'$TO_NUMBER'",
   "from": "'$WHATSAPP_NUMBER'",
-  "channel": "whatsapp",
-  "whatsapp": {
-    "policy": "deterministic",
-    "locale": "en-US"
+  "channel": "whatsapp"
+  "custom": {
+    "type": "template",
+    "template": {
+      "namespace": "$WHATSAPP_TEMPLATE_NAMESPACE",
+      "name": "$WHATSAPP_TEMPLATE_NAME",
+      "language": {
+        "policy": "deterministic",
+        "code": "en"
+      },
+      "components": [
+        {
+          "type": "header",
+          "parameters": [
+            {
+              "type": "location",
+              "location": {
+                "longitude": -122.425332,
+                "latitude": 37.758056,
+                "name": "Facebook HQ",
+                "address": "1 Hacker Way, Menlo Park, CA 94025"
+              }
+            }
+          ]
+        },
+        {
+          "type": "body",
+          "parameters": [
+            {
+              "type": "text",
+              "text": "Value 1"
+            },
+            {
+              "type": "text",
+              "text": "Value 2"
+            },
+            {
+              "type": "text",
+              "text": "Value 3"
+            }
+          ]
+        }
+      ]
+    }
   }
 }'
