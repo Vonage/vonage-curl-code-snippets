575851b81b2e914418344d41e77ee3f6d3523dfb
superchilled <karl@superchilled.co.uk>
2022-02-08 15:09:10 +0000

    Updating send-image example in line with Messages v1 format

--- messages/whatsapp/send-image.sh
+++ messages/whatsapp/send-image.sh
@@ -8,15 +8,12 @@ curl -X POST $MESSAGES_API_URL \
   -H 'Content-Type: application/json' \
   -H 'Accept: application/json' \
   -d $'{
-    "from": { "type": "whatsapp", "number": "'$WHATSAPP_NUMBER'" },
-    "to": { "type": "whatsapp", "number": "'$TO_NUMBER'" },
-    "message": {
-      "content": {
-        "type": "image",
-        "image": { 
-          "url": "'$IMAGE_URL'",
-          "caption": "'$IMAGE_CAPTION'"
-        }
-      }
-    }
-  }'
+  "message_type": "image",
+  "image": {
+    "url": "'$IMAGE_URL'",
+    "caption": "'$IMAGE_CAPTION'"
+  },
+  "to": "'$TO_NUMBER'",
+  "from": "'$WHATSAPP_NUMBER'",
+  "channel": "whatsapp"
+}'
