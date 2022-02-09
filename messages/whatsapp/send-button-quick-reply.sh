81c479b555a0d1438a6c72e8cb60b09ef05828d4
superchilled <karl@superchilled.co.uk>
2022-02-09 14:50:03 +0000

    Updating send-button-quick-reply example in line with Messages v1 format

--- messages/whatsapp/send-button-quick-reply.sh
+++ messages/whatsapp/send-button-quick-reply.sh
@@ -7,78 +7,69 @@ curl -X POST $MESSAGES_API_URL \
   -H 'Authorization: Bearer' $JWT  \
   -H 'Content-Type: application/json' \
   -d '{
-  "from": {
-    "type": "whatsapp",
-    "number": "'$WHATSAPP_NUMBER'"
-  },
-  "to": {
-    "type": "whatsapp",
-    "number": "'$TO_NUMBER'"
-  },
-  "message": {
-    "content": {
-      "type": "custom",
-      "custom": {
-        "type": "template",
-        "template": {
-          "namespace": "'$WHATSAPP_TEMPLATE_NAMESPACE'",
-          "name": "'$WHATSAPP_TEMPLATE_NAME'",
-          "language": {
-            "code": "en",
-            "policy": "deterministic"
-          },
-          "components": [
+  "message_type": "custom",
+  "to": "'$TO_NUMBER'",
+  "from": "'$WHATSAPP_NUMBER'",
+  "channel": "whatsapp"
+  "custom": {
+    "type": "template",
+    "template": {
+      "namespace": "'$WHATSAPP_TEMPLATE_NAMESPACE'",
+      "name": "'$WHATSAPP_TEMPLATE_NAME'",
+      "language": {
+        "code": "en",
+        "policy": "deterministic"
+      },
+      "components": [
+        {
+          "type": "header",
+          "parameters": [
             {
-              "type": "header",
-              "parameters": [
-                {
-                  "type": "text",
-                  "text": "12/26"
-                }
-              ]
+              "type": "text",
+              "text": "12/26"
+            }
+          ]
+        },
+        {
+          "type": "body",
+          "parameters": [
+            {
+              "type": "text",
+              "text": "*Ski Trip*"
             },
             {
-              "type": "body",
-              "parameters": [
-                {
-                  "type": "text",
-                  "text": "*Ski Trip*"
-                },
-                {
-                  "type": "text",
-                  "text": "2019-12-26"
-                },
-                {
-                  "type": "text",
-                  "text": "*Squaw Valley Ski Resort, Tahoe*"
-                }
-              ]
+              "type": "text",
+              "text": "2019-12-26"
             },
             {
-              "type": "button",
-              "sub_type": "quick_reply",
-              "index": 0,
-              "parameters": [
-                {
-                  "type": "payload",
-                  "payload": "Yes-Button-Payload"
-                }
-              ]
-            },
+              "type": "text",
+              "text": "*Squaw Valley Ski Resort, Tahoe*"
+            }
+          ]
+        },
+        {
+          "type": "button",
+          "sub_type": "quick_reply",
+          "index": 0,
+          "parameters": [
             {
-              "type": "button",
-              "sub_type": "quick_reply",
-              "index": 1,
-              "parameters": [
-                {
-                  "type": "payload",
-                  "payload": "No-Button-Payload"
-                }
-              ]
+              "type": "payload",
+              "payload": "Yes-Button-Payload"
+            }
+          ]
+        },
+        {
+          "type": "button",
+          "sub_type": "quick_reply",
+          "index": 1,
+          "parameters": [
+            {
+              "type": "payload",
+              "payload": "No-Button-Payload"
             }
           ]
         }
-      }
+      ]
     }
   }
 }'
