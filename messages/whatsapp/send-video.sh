0b3afb756ed6702c2b0c905909bbafa4c627e634
superchilled <karl@superchilled.co.uk>
2022-02-10 13:48:55 +0000

    Updating WhatsApp send-video code example in line with Messages v1

--- messages/whatsapp/send-video.sh
+++ messages/whatsapp/send-video.sh
@@ -8,15 +8,12 @@ curl -X POST $MESSAGES_API_URL \
   -H 'Content-Type: application/json' \
   -H 'Accept: application/json' \
   -d $'{
-    "from": { "type": "whatsapp", "number": "'$WHATSAPP_NUMBER'" },
-    "to": { "type": "whatsapp", "number": "'$TO_NUMBER'" },
-    "message": {
-      "content": {
-        "type": "video",
-        "video": { 
-          "url": "'$VIDEO_URL'",
-          "caption": "'$VIDEO_CAPTION'"
-        }
-      }
-    }
-  }'
+  "message_type": "video",
+  "video": {
+    "url": "'$VIDEO_URL'",
+    "caption": "'$VIDEO_CAPTION'"
+  },
+  "to": "'$TO_NUMBER'",
+  "from": "'$WHATSAPP_NUMBER'",
+  "channel": "whatsapp"
+}'
