a4e1c3f512c8786264efb1088ddad4c9240968c3
superchilled <karl@superchilled.co.uk>
2022-02-10 13:42:53 +0000

    Updating WhatsApp send-button-link code example in line with Messages v1

--- messages/whatsapp/send-button-link.sh
+++ messages/whatsapp/send-button-link.sh
@@ -7,73 +7,64 @@ curl -X POST $MESSAGES_API_URL \
   -H 'Authorization: Bearer' $JWT  \
   -H 'Content-Type: application/json' \
   -d '{
-  "from": {
-    "type": "whatsapp",
-    "number": "'$WHATSAPP_NUMBER'"
-  },
-  "to": {
-    "type": "whatsapp",
-    "number": "'$TO_NUMBER'"
-  },
-  "message": {
-    "content": {
-      "type": "custom",
-      "custom": {
-        "type": "template",
-        "template": {
-          "namespace": "'$WHATSAPP_TEMPLATE_NAMESPACE'",
-          "name": "'$WHATSAPP_TEMPLATE_NAME'",
-          "language": {
-            "code": "en",
-            "policy": "deterministic"
-          },
-          "components": [
+  "message_type": "custom",
+  "to": "'$TO_NUMBER'",
+  "from": "'$WHATSAPP_NUMBER'",
+  "channel": "whatsapp",
+  "custom": {
+    "type": "template",
+    "template": {
+      "namespace": "'$WHATSAPP_TEMPLATE_NAMESPACE'",
+      "name": "'$WHATSAPP_TEMPLATE_NAME'",
+      "language": {
+        "code": "en",
+        "policy": "deterministic"
+      },
+      "components": [
+        {
+          "type": "header",
+          "parameters": [
             {
-              "type": "header",
-              "parameters": [
-                {
-                  "type": "image",
-                  "image": {
-                    "link": "'$HEADER_IMAGE_URL'"
-                  }
-                }
-              ]
+              "type": "image",
+              "image": {
+                "link": "'$HEADER_IMAGE_URL'"
+              }
+            }
+          ]
+        },
+        {
+          "type": "body",
+          "parameters": [
+            {
+              "type": "text",
+              "text": "Anand"
             },
             {
-              "type": "body",
-              "parameters": [
-                {
-                  "type": "text",
-                  "text": "Anand"
-                },
-                {
-                  "type": "text",
-                  "text": "Quest"
-                },
-                {
-                  "type": "text",
-                  "text": "113-0921387"
-                },
-                {
-                  "type": "text",
-                  "text": "23rd Nov 2019"
-                }
-              ]
+              "type": "text",
+              "text": "Quest"
             },
             {
-              "type": "button",
-              "index": "0",
-              "sub_type": "url",
-              "parameters": [
-                {
-                  "type": "text",
-                  "text": "1Z999AA10123456784"
-                }
-              ]
+              "type": "text",
+              "text": "113-0921387"
+            },
+            {
+              "type": "text",
+              "text": "23rd Nov 2019"
+            }
+          ]
+        },
+        {
+          "type": "button",
+          "index": "0",
+          "sub_type": "url",
+          "parameters": [
+            {
+              "type": "text",
+              "text": "1Z999AA10123456784"
             }
           ]
         }
-      }
+      ]
     }
   }
 }'
