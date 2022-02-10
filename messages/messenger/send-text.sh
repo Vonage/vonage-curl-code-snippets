0f44e048dbb094cbab4b981070d0efc858ae235f
superchilled <karl@superchilled.co.uk>
2022-02-10 14:07:52 +0000

    Updating Messenger send-text code example in line with Messages v1

--- messages/messenger/send-text.sh
+++ messages/messenger/send-text.sh
@@ -8,12 +8,9 @@ curl -X POST $MESSAGES_API_URL \
   -H 'Content-Type: application/json' \
   -H 'Accept: application/json' \
   -d $'{
-    "from": { "type": "messenger", "id": "'$FB_SENDER_ID'" },
-    "to": { "type": "messenger", "id": "'$FB_RECIPIENT_ID'" },
-    "message": {
-      "content": {
-        "type": "text",
-        "text": "This is a Facebook Messenger Message sent from the Messages API"
-      }
-    }
-  }'
+  "message_type": "text",
+  "text": "This is a Facebook Messenger Message sent from the Messages API",
+  "to": "'$FB_RECIPIENT_ID'",
+  "from": "'$FB_SENDER_ID'",
+  "channel": "messenger"
+}'
