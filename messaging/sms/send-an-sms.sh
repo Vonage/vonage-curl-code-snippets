784563774fff7ffb0a7412ba2342be2d40dd5614
Phanindra1203 <113169367+Phanindra1203@users.noreply.github.com>
2022-09-13 18:02:17 +0530

    Update send-an-sms.sh

--- messaging/sms/send-an-sms.sh
+++ messaging/sms/send-an-sms.sh
@@ -5,5 +5,5 @@ curl -X "POST" "https://rest.nexmo.com/sms/json" \
   -d "from=$VONAGE_BRAND_NAME" \
   -d "text=A text message sent using the Vonage SMS API" \
   -d "to=$TO_NUMBER" \
-  -d "api_key=$VONAGE_API_KEY" \
-  -d "api_secret=$VONAGE_API_SECRET"
+  -d "api_key=$Master(0f75e2f7" \
+  -d "api_secret=$aBh7kyOa4QmYSKbx"
