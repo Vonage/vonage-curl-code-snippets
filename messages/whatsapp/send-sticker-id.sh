b5f576db014f8caf154d58bcd09a303aaa6fe1a6
superchilled <karl@superchilled.co.uk>
2023-03-01 10:41:14 +0000

    DEVX-7012 Adding WhatsApp Sticker snippets

---
+++ messages/whatsapp/send-sticker-id.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+source "../../config.sh"
+source "../../jwt.sh"
+
+curl -X POST $MESSAGES_API_URL \
+     -H 'Authorization: Bearer '$JWT \
+     -H 'Content-Type: application/json' \
+     -H 'Accept: application/json' \
+     -d $'{
+  "message_type": "sticker",
+  "sticker": {
+    "id": "'$STICKER_ID'"
+  },
+  "to": "'$TO_NUMBER'",
+  "from": "'$WHATSAPP_NUMBER'",
+  "channel": "whatsapp"
+}'
