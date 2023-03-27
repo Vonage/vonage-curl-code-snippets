b4ea132333a76a2b86f1ef9d38bdd9045ec961b4
Sina Madani <sina.madani@vonage.com>
2023-03-27 15:51:21 +0100

    Added PSIP digest auth params

--- voice/sip/create-domain.sh
+++ voice/sip/create-domain.sh
@@ -4,5 +4,5 @@ source "../../config.sh"
 MY_IP_ADDRESS=$(curl ifconfig.co)
 
 curl -X POST -H "Content-Type: application/json" \
-    -d '{"name": "'$SIP_DOMAIN'", "application_id": "'$VONAGE_APPLICATION_ID'", "acl": ["'$MY_IP_ADDRESS'"]}' \
+    -d '{"name": "'$SIP_DOMAIN'", "application_id": "'$VONAGE_APPLICATION_ID'", "acl": ["'$MY_IP_ADDRESS'"], "srtp": "optional", "tls": "optional", "digest_auth": false}' \
     https://api.nexmo.com/v1/psip -u $VONAGE_API_KEY:$VONAGE_API_SECRET
--- voice/sip/update-domain.sh
+++ voice/sip/update-domain.sh
@@ -2,5 +2,5 @@
 source "../../config.sh"
 
 curl -X PUT -H "Content-Type: application/json" \
-    -d '{"name": "'$SIP_DOMAIN'", "application_id": "'$VONAGE_APPLICATION_ID'", "acl": ["188.180.180.180", "77.77.77.0/24"]}' \
+    -d '{"name": "'$SIP_DOMAIN'", "application_id": "'$VONAGE_APPLICATION_ID'", "acl": ["0.0.0.0/0"], "tls": "always", "srtp": "never", "digest_auth": true}' \
     https://api.nexmo.com/v1/psip/$SIP_DOMAIN -u $VONAGE_API_KEY:$VONAGE_API_SECRET
