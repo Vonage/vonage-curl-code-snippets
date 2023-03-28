9e0000141551753bb96075599b61d7f06e01e7f1
Sina Madani <sina.madani@vonage.com>
2023-03-28 11:47:27 +0100

    Added --fail-with-body to requests

--- voice/sip/delete-domain-user.sh
+++ voice/sip/delete-domain-user.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source "../../config.sh"
 
-curl -X DELETE https://api.nexmo.com/v1/psip/$SIP_DOMAIN/users/$SIP_USER_ID -u $VONAGE_API_KEY:$VONAGE_API_SECRET
+curl --fail-with-body -X DELETE https://api.nexmo.com/v1/psip/$SIP_DOMAIN/users/$SIP_USER_ID -u $VONAGE_API_KEY:$VONAGE_API_SECRET
--- voice/sip/update-domain.sh
+++ voice/sip/update-domain.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source "../../config.sh"
 
-curl -X PUT -H "Content-Type: application/json" \
+curl --fail-with-body -X PUT -H "Content-Type: application/json" \
     -d '{"name": "'$SIP_DOMAIN'", "application_id": "'$VONAGE_APPLICATION_ID'", "acl": ["0.0.0.0/0"], "tls": "always", "srtp": "never", "digest_auth": true}' \
     https://api.nexmo.com/v1/psip/$SIP_DOMAIN -u $VONAGE_API_KEY:$VONAGE_API_SECRET
