033c650c27c48e1a0a86c17c6fef3847a68bd191
James Seconde <jim.seconde@googlemail.com>
2023-05-24 15:58:01 +0100

    add whatsapp and whatsapp i

---
+++ verify2/send-request-whatsapp-interactive.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+source "../config.sh"
+source "../jwt.sh"
+
+curl --X POST \
+  --url https://api.nexmo.com/v2/verify \
+  -H "Authorization: Bearer "$JWT\
+  -H 'Content-Type: application/json' \
+  -d $'{
+	 "brand": "'$BRAND_NAME'",
+   "workflow": [
+      {
+         "channel": "whatsapp_interactive",
+         "to": "'$TO_NUMBER'"
+      },
+   ]
+}'
