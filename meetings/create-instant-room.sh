e161fa8dc1ef4b31d3ff7c34f13ca93f6b6eea22
tr00d <guillaume.faas@live.fr>
2023-07-04 11:55:29 +0200

    Add snippets for Meetings API

---
+++ meetings/create-instant-room.sh
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env bash
+source "../config.sh"
+source "../jwt.sh"
+  
+curl --X POST \
+  --url https://api-eu.vonage.com/meetings/rooms \
+  -H "Authorization: Bearer "$JWT \
+  -H "Content-Type: application/json" \
+  -d '{
+      "display_name":"'ROOM_DISPLAY_NAME'"
+   }'
