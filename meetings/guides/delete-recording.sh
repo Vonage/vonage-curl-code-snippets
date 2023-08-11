80ee31185b628bc643c71178c12587870bf6f822
tr00d <guillaume.faas@live.fr>
2023-08-11 14:39:24 +0200

    Remove /meetings/guides and move /snippets one level above

--- meetings/guides/delete-recording.sh
+++
@@ -1,6 +0,0 @@
-﻿#!/usr/bin/env bash
-source "../config.sh"
-source "../jwt.sh"
-
-curl -X DELETE 'https://api-eu.vonage.com/meetings/recordings/9f6fe8ae-3458-4a72-b532-8276d5533e97'
--H 'Authorization: Bearer '$JWT
--- meetings/guides/delete-theme.sh
+++
@@ -1,5 +0,0 @@
-﻿#!/usr/bin/env bash
-source "../config.sh"
-source "../jwt.sh"
-
-curl -X DELETE 'https://api-eu.vonage.com/meetings/themes/e8b1d80b-8f78-4578-94f2-328596e01387?force=true'
