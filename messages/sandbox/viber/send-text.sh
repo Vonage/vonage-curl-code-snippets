192e65e8fe8a4c8e7449cc02edf03aadaa783b7f
superchilled <karl@superchilled.co.uk>
2024-04-03 11:11:57 +0100

    Updating Viber Sanbox variable

--- messages/sandbox/viber/send-text.sh
+++ messages/sandbox/viber/send-text.sh
@@ -7,7 +7,7 @@ curl -X POST $MESSAGES_SANDBOX_URL \
   -H 'Content-Type: application/json' \
   -H 'Accept: application/json' \
   -d '{
-    "from": { "type": "viber_service_msg", "id": "'$MESSAGES_SANDBOX_VIBER_ID'" },
+    "from": { "type": "viber_service_msg", "id": "'$MESSAGES_SANDBOX_VIBER_SERVICE_ID'" },
     "to": { "type": "viber_service_msg", "number": "'$MESSAGES_SANDBOX_ALLOW_LISTED_TO_NUMBER'" },
     "message": {
       "content": {
