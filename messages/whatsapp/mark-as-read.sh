3fbde1f1f819236e11b58cea591c425c189dfe87
Karl Lingiah <karl@superchilled.co.uk>
2024-08-20 10:45:31 +0100

    Add WhatsApp mark as read snippet (#135)

---
+++ messages/whatsapp/mark-as-read.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+source "../../config.sh"
+source "../../jwt.sh"
+
+curl -X PATCH $GEOSPECIFIC_MESSAGES_API_URL'/'$MESSAGE_UUID \
+     -H 'Authorization: Bearer '$JWT \
+     -H 'Content-Type: application/json' \
+     -H 'Accept: application/json' \
+     -d $'{"status": "read"}'
