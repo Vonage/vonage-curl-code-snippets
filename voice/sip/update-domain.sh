6f1e3373eb284ab4b3ad5a51465b5f938333e2d4
Sina Madani <sina.madani@vonage.com>
2025-01-23 11:51:34 +0000

    Newlines in PSIP

--- voice/sip/update-domain.sh
+++ voice/sip/update-domain.sh
@@ -2,5 +2,6 @@
 source "../../config.sh"
 
 curl --fail-with-body -X PUT -H "Content-Type: application/json" \
-    -d '{"name": "'$SIP_DOMAIN'", "application_id": "'$VONAGE_APPLICATION_ID'", "acl": ["0.0.0.0/0"], "tls": "always", "srtp": "never", "digest_auth": true}' \
-    https://api.nexmo.com/v1/psip/$SIP_DOMAIN -u $VONAGE_API_KEY:$VONAGE_API_SECRET
+  -d '{"name": "'$SIP_DOMAIN'", "application_id": "'$VONAGE_APPLICATION_ID \
+  '", "acl": ["0.0.0.0/0"], "tls": "always", "srtp": "never", "digest_auth": true}' \
+  https://api.nexmo.com/v1/psip/$SIP_DOMAIN -u $VONAGE_API_KEY:$VONAGE_API_SECRET
