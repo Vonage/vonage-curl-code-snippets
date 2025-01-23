49fd47e59b5f834319658b7f024799fd29fa103e
Sina Madani <sina.madani@vonage.com>
2025-01-23 11:58:48 +0000

    feat: Add PSIP query-domains (#146)
    * feat: Add PSIP query-domains
    
    * Move voice variables up on config.sh
    
    * Newlines in PSIP

--- voice/sip/query-domains.sh
+++ voice/sip/query-domains.sh
@@ -2,5 +2,5 @@
 source "../../config.sh"
 
 curl --fail-with-body -X GET \
-  https://api.nexmo.com/v1/psip/$SIP_DOMAIN/users/$SIP_USER_ID \
+  https://api.nexmo.com/v1/psip \
   -u $VONAGE_API_KEY:$VONAGE_API_SECRET
