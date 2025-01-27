4ff9251e78d81d060ff5d74ab934dabbdd8f8126
superchilled <karl@superchilled.co.uk>
2025-01-27 12:32:46 +0000

    Updating Messages SMS snippets

--- messages/sms/send-sms.sh
+++ messages/sms/send-sms.sh
@@ -4,12 +4,12 @@ source "../../config.sh"
 source "../../jwt.sh"
 
 curl -X POST https://api.nexmo.com/v1/messages \
-  -H 'Authorization: Bearer '$JWT\
+  -H "Authorization: Bearer ${JWT}"\
   -H 'Content-Type: application/json' \
   -H 'Accept: application/json' \
   -d $'{
-    "to": "'$TO_NUMBER'",
-    "from": "'$FROM_NUMBER'",
+    "to": "'${MESSAGES_TO_NUMBER}'",
+    "from": "'${SMS_SENDER_ID}'",
     "channel": "sms",
     "message_type": "text",
     "text": "This is an SMS sent using the Vonage Messages API."
--- sms/send-an-sms.sh
+++ sms/send-an-sms.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source '../config.sh'
 
-curl -X POST 'https://rest.nexmo.com/sms/json' \
+curl -X POST https://rest.nexmo.com/sms/json \
   -d "from=${SMS_SENDER_ID}" \
   -d "to=${SMS_TO_NUMBER}" \
   -d 'text=A text message sent using the Vonage SMS API' \
