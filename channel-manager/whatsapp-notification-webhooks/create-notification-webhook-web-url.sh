553873faefbe0df5b4eccbe4450666fff99d531a
superchilled <karl@superchilled.co.uk>
2025-06-18 12:43:46 +0100

    Fixing indentation (again!)

--- channel-manager/whatsapp-notification-webhooks/create-notification-webhook-web-url.sh
+++ channel-manager/whatsapp-notification-webhooks/create-notification-webhook-web-url.sh
@@ -3,12 +3,12 @@
 source "../../config.sh"
 
 curl -X POST https://api.nexmo.com/v2/whatsapp-manager/subscriptions \
-	-u "${VONAGE_API_KEY}:${VONAGE_API_SECRET}" \
-	-H 'Content-Type: application/json' \
-	-H 'Accept: application/json' \
-	-d $'{
-		"type": "webhook",
+  -u "${VONAGE_API_KEY}:${VONAGE_API_SECRET}" \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: application/json' \
+  -d $'{
+    "type": "webhook",
     "webhook": {
       "url": "'${CHANNEL_MANAGER_WHATSAPP_NOTIFICATION_WEBHOOK_URL_WEB}'"
     }
-	}'
+  }'
