ccdd385ae995b010e80e0a84b5a9708bf0c69005
Karl Lingiah <karl@superchilled.co.uk>
2025-09-26 16:33:53 +0100

    feat: Adding/Updating WhatsApp Analytics snippets

--- channel-manager/whatsapp-analytics/get-template-analytics.sh
+++ channel-manager/whatsapp-analytics/get-template-analytics.sh
@@ -2,9 +2,9 @@
 
 source "../../config.sh"
 
-curl -G https://api.nexmo.com/v1/channel-manager/whatsapp/wabas/"${CHANNEL_MANAGER_WABA_ID}"/messaging-analytics \
+curl -G https://api.nexmo.com/v1/channel-manager/whatsapp/wabas/"${CHANNEL_MANAGER_WABA_ID}"/template-analytics \
   -u "${VONAGE_API_KEY}:${VONAGE_API_SECRET}" \
   -H 'Accept: application/json' \
   --data-urlencode start=$CHANNEL_MANAGER_WHATSAPP_ANALYTICS_START \
   --data-urlencode end=$CHANNEL_MANAGER_WHATSAPP_ANALYTICS_END \
-  --data-urlencode granularity=$CHANNEL_MANAGER_WHATSAPP_ANALYTICS_GRANULARITY
+  --data-urlencode template_ids=$CHANNEL_MANAGER_WHATSAPP_ANALYTICS_TEMPLATE_IDS
