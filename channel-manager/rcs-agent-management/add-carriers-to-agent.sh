aa59ae657d98c57123526616fbfdda59ed3a0fed
superchilled <karl@superchilled.co.uk>
2025-10-01 16:59:14 +0100

    Adding RCS Agent management snippets

--- channel-manager/rcs-agent-management/add-carriers-to-agent.sh
+++ channel-manager/rcs-agent-management/add-carriers-to-agent.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
 source "../../config.sh"
+
+curl -X POST https://api.nexmo.com/v1/channel-manager/rcs/agents/${CHANNEL_MANAGER_RCS_AGENT_ID}/carriers \
+  -u "${VONAGE_API_KEY}:${VONAGE_API_SECRET}" \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: application/json' \
+  -d $'{
+     "carriers": [
+        "'${CHANNEL_MANAGER_RCS_AGENT_CARRIER_ID}'"
+    ]
+  }'
