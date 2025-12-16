9f6710a7447fba93286f1a0d95f4c70932887a79
Alvaro Navarro <alvaro.navarro@vonage.com>
2025-12-16 13:35:43 +0100

    Remove CAMARA APIs (#161)

--- network-apis/sim-swap/retrieve-sim-swap-date.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "../../config.sh"
-
-curl -X POST https://api-eu.vonage.com/camara/sim-swap/v040/retrieve-date \
-  --header "Authorization: Bearer $VNA_ACCESS_TOKEN" \
-  --header "Content-Type: application/json" \
-  --data '{ "phoneNumber": "'$VNA_PHONE_NUMBER'" }'
