0e5daf093d2c467521a74c7ffaf5e8a15c9f5c4a
superchilled <karl@superchilled.co.uk>
2025-12-17 16:25:31 +0000

    DEVX-10670: Updating SMS snippets to use Basic Auth

--- sms/send-an-sms.sh
+++ sms/send-an-sms.sh
@@ -2,8 +2,7 @@
 source '../config.sh'
 
 curl -X POST https://rest.nexmo.com/sms/json \
+  -u "$VONAGE_API_KEY:$VONAGE_API_SECRET" \
   -d "from=${SMS_SENDER_ID}" \
   -d "to=${SMS_TO_NUMBER}" \
-  -d 'text=A text message sent using the Vonage SMS API' \
-  -d "api_key=${VONAGE_API_KEY}" \
-  -d "api_secret=${VONAGE_API_SECRET}"
+  -d 'text=A text message sent using the Vonage SMS API'
