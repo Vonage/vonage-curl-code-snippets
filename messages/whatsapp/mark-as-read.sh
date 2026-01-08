f8653307d16733ee0ef12dbc5e0bcd002d778082
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2026-01-07 21:12:01 -0500

    feat: updated mark as read for whatsapp

--- messages/whatsapp/mark-as-read.sh
+++ messages/whatsapp/mark-as-read.sh
@@ -7,4 +7,10 @@ curl -X PATCH "${GEOSPECIFIC_MESSAGES_API_URL}/${MESSAGES_MESSAGE_ID}" \
   -H "Authorization: Bearer "$JWT\
   -H 'Content-Type: application/json' \
   -H 'Accept: application/json' \
-  -d $'{"status": "read"}'
+  -d $'{
+    "status": "read",
+    "typing_indicators": {
+      "show": "true",
+      "type": "text"
+    }
+  }'
