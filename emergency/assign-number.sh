fe141aae61e86af7798781df9134969250abeecc
tr00d <guillaume.faas@live.fr>
2026-01-16 16:21:12 +0100

    feat: add missing parameters in Emergency API

--- emergency/assign-number.sh
+++ emergency/assign-number.sh
@@ -8,8 +8,8 @@ curl -X "PATCH" "https://api.nexmo.com/v1/emergency/numbers/$EMERGENCY_NUMBER" \
      -d $'{
         {
            "address": {
-              "id": "c49f3586-9b3b-458b-89fc-3c8beb58865c"
+              "id": "$EMERGENCY_ADDRESS_ID"
            },
-           "contact_name": "John Smith"
+           "contact_name": "$EMERGENCY_CONTACT_NAME"
         }
      }'
