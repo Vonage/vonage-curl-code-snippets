a2b36b3ea116a0e2c23c774defe9ba68c8f6a347
Alvaro Navarro <alvaro.navarro@vonage.com>
2026-03-20 15:14:43 +0100

    feat: add whatsapp zero tap code snippet

---
+++ verify2/send-request-whatsapp-zerotap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+source "../config.sh"
+source "../jwt.sh"
+
+curl -X POST "https://api.nexmo.com/v2/verify" \
+  -H "Authorization: Bearer $JWT"\
+  -H 'Content-Type: application/json' \
+  -d $'{
+	 "brand": "'$VERIFY_BRAND_NAME'",
+   "workflow": [
+      {
+         "channel": "whatsapp",
+         "to": "'$VERIFY_NUMBER'",
+         "from": "'$VERIFY_FROM_NUMBER'",
+         "mode": "zero_tap"
+      }
+   ]
+}'
