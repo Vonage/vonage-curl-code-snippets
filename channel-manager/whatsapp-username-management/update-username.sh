ab97e1c63552f3bc2cef38228e9d6187359c944f
Chuck Reeves <manchuck@users.noreply.github.com>
2026-09-18 13:43:47 -0400

    feat: added WhatsApp Username snippets (#176)

---
+++ channel-manager/whatsapp-username-management/update-username.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source "../../config.sh"
+
+curl -X PATCH https://api.nexmo.com/v1/channel-manager/whatsapp/numbers/${CHANNEL_MANAGER_WABA_NUMBER}/username \
+  -u "${VONAGE_API_KEY}:${VONAGE_API_SECRET}" \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: application/json' \
+  -d $'{
+    "username": "'${CHANNEL_MANAGER_WHATSAPP_USERNAME}'"
+  }'
